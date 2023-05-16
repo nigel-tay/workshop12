306817917554658e08c6f625d2a768ec95a31f9d
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-05-16 15:55:19 +0800

    add for loop for imageList and iterate through to display appropriate number of images

--- src/main/java/workshop12/workshop/controller/RandomNumberGenController.java
+++ src/main/java/workshop12/workshop/controller/RandomNumberGenController.java
@@ -1,16 +1,52 @@
 package workshop12.workshop.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import workshop12.workshop.model.Image;
+import workshop12.workshop.service.RandomNumService;
 
 @Controller
-@RequestMapping(path = "/api")
 public class RandomNumberGenController {
 
+    @Autowired
+    RandomNumService service;
 
     @GetMapping("/home")
     public String landingPage() {
         return "home";
+    }
+
+    @GetMapping("/get")
+    public String generateRandomNumber(Model model, HttpServletRequest request) {
+
+        int number = Integer.parseInt(request.getParameter("number"));
+        System.out.println("Given number: " + number);
+
+        if (number < 1 || number > 30) {
+            String errorMessageString = "Number given: " + number + " is out of range";
+            model.addAttribute("errorMessage", errorMessageString);
+            return "home";
+        }
+
+        // CALLING SERVICE METHOD
+        List<Integer> randomNumbers = service.generateRandomNumber(number);
+
+        // Populate image objects
+        List<Image> imageList = new ArrayList<Image>();
+
+        for (int num: randomNumbers) {
+            imageList.add(new Image(Integer.toString(num), "/images/" + Integer.toString(num) + ".png"));
+        }
+
+        model.addAttribute(imageList);
+
+        return "display";
     }
 }
--- src/main/java/workshop12/workshop/service/RandomNumService.java
+++ src/main/java/workshop12/workshop/service/RandomNumService.java
@@ -1,8 +1,20 @@
 package workshop12.workshop.service;
 
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class RandomNumService {
     
+    public List<Integer> generateRandomNumber(int number) {
+        return new Random()
+                    .ints(1, 31)
+                    .distinct()
+                    .limit(number)
+                    .boxed()
+                    .collect(Collectors.toList());
+    }
 }
